ee60f338bc37fe4f9d5ae81cc5595faceeef1a9a
Jaynil Gaglani <g.jaynil2401@gmail.com>
2024-11-26 22:24:55 +0530

    feat: max product subarray

---
+++ data-structures/dyamic-programming/maximumProductSubarray.js
@@ -0,0 +1,22 @@
+/**
+ * https://takeuforward.org/data-structure/maximum-product-subarray-in-an-array/
+ * TC -> O(N)
+ * SC -> O(1)
+ */
+
+function maximumProductSubarray(array) {
+  const n = array.length;
+  let prefix = 1;
+  let suffix = 1;
+  let max = Number.NEGATIVE_INFINITY;
+
+  for (let i = 0; i < n; i++) {
+    prefix = prefix * array[i];
+    suffix = suffix * array[n - i - 1];
+    max = Math.max(max, prefix, suffix);
+  }
+  return max;
+}
+
+const input = [1, 2, -3, 0, -4, -5];
+console.log(maximumProductSubarray(input));
